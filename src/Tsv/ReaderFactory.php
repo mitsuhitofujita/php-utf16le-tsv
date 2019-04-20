6126ff8c188129e1b330090989117fad8ed91cbf
mitsuhitofujita <mitsuhitofujita@gmail.com>
2019-04-20 12:14:51 +0900

    1 Imprement

---
+++ src/Tsv/ReaderFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tsv;
+
+use SplFileObject;
+
+class ReaderFactory
+{
+    /**
+     * @param string $filename
+     * @return SplFileObject
+     */
+	public static function create(string $filename): SplFileObject
+	{
+		$filtered = "php://filter/read=convert.iconv.utf-16%2Futf-8/resource=$filename";
+		$file = new SplFileObject($filtered, 'rb');
+		$file->setFlags(SplFileObject::READ_CSV | SplFileObject::SKIP_EMPTY | SplFileObject::READ_AHEAD);
+		$file->setCsvControl("\t");
+		return $file;
+	}
+}
